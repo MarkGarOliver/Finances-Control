0bc906a91fb3f96b99bdfebd76ee7094a10d8d0c
MarkGarOliver <marcosgarcia.oliveira02@gmail.com>
2022-05-25 19:24:45 -0300

    exibição de acordo com o mês funcionando

--- src/components/ResumeBar.tsx
+++ src/components/ResumeBar.tsx
@@ -9,6 +9,8 @@ import { ChevronDownIcon } from '@heroicons/react/solid'
 
 type Props = {
     registros: Array<Registros>
+    onChangeData?: React.ChangeEventHandler
+    onClickBuscar: React.MouseEventHandler | undefined;
 };
 
 function classNames(...classes: string[]) {
@@ -18,7 +20,7 @@ function classNames(...classes: string[]) {
 
 const ResumeBar = (props: Props) => {
 
-    const { registros } = props
+    const { registros, onChangeData, onClickBuscar } = props
 
     var despesas = 0
     var receitas = 0
@@ -34,119 +36,33 @@ const ResumeBar = (props: Props) => {
 
     return (
         <div className='flex justify-around bg-slate-800 p-11 mb-10 border border-slate-800 rounded-xl shadow'>
-            <div className='flex w-96 justify-around mt-auto mb-auto '>
 
-                <a className='bg-slate-700 border-slate-700 p-2 border rounded-xl cursor-pointer'>
-                    back
-                </a>
+            <div className='mt-auto mb-auto' id='dropdown'>
 
-                <p>
-                    Janeiro 2020
-                </p>
 
-                <a className='bg-slate-700 border-slate-700 p-2 border rounded-xl cursor-pointer'>
-                    next
-                </a>
+                <input onChange={onChangeData} className='rounded-lg w-64 p-2 font-semibold text-black' type="month" id="bdaymonth" name="bdaymonth" /> 
+
+                <button onClick={onClickBuscar} className=' ml-2 bg-slate-700 w-32 h-11 mt-9 rounded-lg hover:bg-slate-600'>Buscar</button>
 
             </div>
 
-            <div className='text-center'>
+            <div className='text-center mt-auto mb-auto'>
                 <label className='uppercase text-lg' htmlFor="Receita">Receita</label>
                 <p className='text-xl mt-2 font-semibold'>R${receitas}</p>
             </div>
 
 
-            <div>
+            <div className='text-center mt-auto mb-auto'>
                 <label className='uppercase text-lg' htmlFor="despesas">Despesas</label>
                 <p className='text-xl mt-2 font-semibold '>R${despesas}</p>
             </div>
 
 
-            <div>
+            <div className='text-center mt-auto mb-auto'>
                 <label className='uppercase text-lg' htmlFor="Receita">Balanço</label>
                 <p className='text-xl mt-2 font-semibold'>R${receitas - despesas}</p>
             </div>
 
-            <div id='dropdown'>
-
-                <Menu as="div" className="relative inline-block text-left">
-                    <div>
-                        <Menu.Button className="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500">
-                            Options
-                            <ChevronDownIcon className="-mr-1 ml-2 h-5 w-5" aria-hidden="true" />
-                        </Menu.Button>
-                    </div>
-
-                    <Transition
-                        as={Fragment}
-                        enter="transition ease-out duration-100"
-                        enterFrom="transform opacity-0 scale-95"
-                        enterTo="transform opacity-100 scale-100"
-                        leave="transition ease-in duration-75"
-                        leaveFrom="transform opacity-100 scale-100"
-                        leaveTo="transform opacity-0 scale-95"
-                    >
-                        <Menu.Items className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
-                            <div className="py-1">
-                                <Menu.Item>
-                                    {({ active }) => (
-                                        <a
-                                            href="#"
-                                            className={classNames(
-                                                active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                                                'block px-4 py-2 text-sm'
-                                            )}
-                                        >
-                                            Account settings
-                                        </a>
-                                    )}
-                                </Menu.Item>
-                                <Menu.Item>
-                                    {({ active }) => (
-                                        <a
-                                            href="#"
-                                            className={classNames(
-                                                active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                                                'block px-4 py-2 text-sm'
-                                            )}
-                                        >
-                                            Support
-                                        </a>
-                                    )}
-                                </Menu.Item>
-                                <Menu.Item>
-                                    {({ active }) => (
-                                        <a
-                                            href="#"
-                                            className={classNames(
-                                                active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                                                'block px-4 py-2 text-sm'
-                                            )}
-                                        >
-                                            License
-                                        </a>
-                                    )}
-                                </Menu.Item>
-                                <form method="POST" action="#">
-                                    <Menu.Item>
-                                        {({ active }) => (
-                                            <button
-                                                type="submit"
-                                                className={classNames(
-                                                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                                                    'block w-full text-left px-4 py-2 text-sm'
-                                                )}
-                                            >
-                                                Sign out
-                                            </button>
-                                        )}
-                                    </Menu.Item>
-                                </form>
-                            </div>
-                        </Menu.Items>
-                    </Transition>
-                </Menu>
-            </div>
 
         </div>
     );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -22,67 +22,104 @@ const Home = () => {
     const [Data, setData] = useState<string>('');
     const [Categoria, setCategoria] = useState<string>('Receita');
     const [Registros, setRegistros] = useState<Registros[]>([]);
+    const [dataView, setdataView] = useState<string>('');
+    const [RegistrosFiltrados, setRegistrosFiltrados] = useState<Registros[]>([]);
 
+    // chamadas supabase
+        const NewRegistro = async () => {
 
-    const NewRegistro = async () => {
+            const { data, error } = await supabase
+                .from('despesas')
+                .insert([
+                    { user_id: 2157, titulo: Titulo, valor: Valor, categoria: Categoria, data: Data },
+                ])
 
-        const { data, error } = await supabase
-            .from('despesas')
-            .insert([
-                { user_id: 2157, titulo: Titulo, valor: Valor, categoria: Categoria, data: Data },
-            ])
-
-        if (error) {
-            alert('erro ao salvar')
-        } else {
-            alert('salvo com sucesso')
+            if (error) {
+                alert('erro ao salvar')
+            } else {
+                alert('salvo com sucesso')
+            }
         }
-    }
 
-
-
-    const GetData = async () => {
-
-        const { data } = await supabaseAxios.get('/despesas?user_id=eq.2157&select=*')
-        setRegistros(data)
-
-    }
-
-
-    const getTitulo = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setTitulo(event.target.value)
-        console.log(Titulo);
-
-    }
-
-
-    const getValor = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setValor(event.target.value)
-        console.log(Valor);
-
-    }
-
-    const getData = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setData(event.target.value)
-        console.log(Data);
-
-    }
-
-    const getCategoria = (event: React.ChangeEvent<HTMLInputElement>) => {
-        if (event.target.checked === true) {
-            setCategoria('Despesa')
+        const GetRegistros = async () => {
+            const { data } = await supabaseAxios.get('/despesas?user_id=eq.2157&select=*')
+            setRegistros(data)
         }
-    }
+
+        const Filtrar = () => {
+
+            var dados: Registros[] = []
+
+            Registros.map((item)=>{
+                
+                const strData = item.data.substring(0, item.data.length - 3);
+
+                const dataV = `${dataView}`
+                
+                
+                if(strData == dataV){
+                    dados.push(item)
+                    
+                    
+                    
+                }
+
+                
+            })
+
+            console.log(dados);
+            
+           setRegistrosFiltrados(dados)
+           console.log(RegistrosFiltrados);
+           
+            
+            
+        
+        }
+
+    // captura de dados 
+        const getTitulo = (event: React.ChangeEvent<HTMLInputElement>) => {
+            setTitulo(event.target.value)
+
+        }
+
+        const getDataView = (event: React.ChangeEvent<HTMLInputElement>) => {
+            setdataView(event.target.value)
+            console.log(dataView);
+            
+        }
+
+
+        const getValor = (event: React.ChangeEvent<HTMLInputElement>) => {
+            setValor(event.target.value)
+            console.log(Valor);
+
+        }
+
+        const getData = (event: React.ChangeEvent<HTMLInputElement>) => {
+            setData(event.target.value)
+            console.log(Data);
+
+        }
+
+        const getCategoria = (event: React.ChangeEvent<HTMLInputElement>) => {
+            if (event.target.checked === true) {
+                setCategoria('Despesa')
+            }
+        }
 
     React.useEffect(() => {
-        GetData()
+        GetRegistros()
     })
+
     return (
         <div className="text-white bg-slate-900 h-screen p-11">
             <h1 className='font-bold uppercase text-xl text-center mb-10'>Finances Control</h1>
 
             <ResumeBar 
-                registros={Registros}
+                registros={RegistrosFiltrados}
+                onChangeData={getDataView}
+                onClickBuscar={Filtrar}
             />
 
             <InputData
@@ -96,7 +133,7 @@ const Home = () => {
             <div className='mt-10'>
 
                 <DataView
-                    registros={Registros}
+                    registros={RegistrosFiltrados}
                 />
             </div>
 
